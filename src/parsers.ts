ea7700c74fc9ce3231539b47666ec1edd1590f32
Allison Casey <alliecasey21@gmail.com>
2019-10-02 19:44:57 -0400

    added indent_level to basic parser reducer

--- src/parsers.ts
+++ src/parsers.ts
@@ -4,10 +4,10 @@ export namespace Parsers {
   export type BasicSetting = [string, string];
 
   export const parseBasicSettings = (settings: BasicSetting[]): string[] => {
-    let indent_level: number = 0;
-    let output: string[];
 
-      const parse = (acc: string[], [key, value]: BasicSetting): string[] => {
+      type Reducer = {indent_level: number, arr: string[]};
+
+      const parse = ({ indent_level, arr }: Reducer, [key, value]: BasicSetting): Reducer => {
         if (key && value == "") {
             let out: string[];
             if (indent_level > 1) {
@@ -16,18 +16,18 @@ export namespace Parsers {
                 out = [`Class ${key} {`]
                 indent_level++;
             }
-            return [...acc, ...out]
+            return {indent_level, arr: [...arr, ...out] }
         } else if (key == "" && value == "") {
             const dedents: string[] = Utils.fillArray(indent_level, "};")
             indent_level = 0;
-            return [...acc, ...dedents];
+            return {indent_level, arr: [...arr, ...dedents] };
         } else {
-            return [...acc, `${key} = ${value};`];
+            return {indent_level, arr: [...arr, `${key} = ${value};`] };
         }
       }
-      output = settings.reduce(parse, []);
-      output = [...output, ...Utils.fillArray(indent_level, "};")]
+      let {indent_level, arr} = settings.reduce(parse, {indent_level: 0, arr: []});
+      arr = [...arr, ...Utils.fillArray(indent_level, "};")]
 
-    return output;
+    return arr;
   };
 }
